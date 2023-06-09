016ede867207c0f11c6cc3050c1ed9eddffa1d9c
Ali Tourani <a.tourani1991@gmail.com>
2023-06-09 15:18:55 +0200

    [fix] - separate some modules

--- vision/postProcessing.py
+++ vision/postProcessing.py
@@ -1,37 +1,6 @@
 import cv2 as cv
 import numpy as np
-
-
-def applyCircularMask(image: np.ndarray, coverage: float = 1.0):
-    """
-    Filters a region of interest from an image based on the provided configurations.
-    The main functionality is to remove circular obstacles in the image hardware setup.
-
-    Parameters:
-    -----------
-    image: numpy.ndarray
-        Image to be filtered
-    coverage: float
-        The coverage of the circle diameter
-
-    Returns:
-    --------
-    mask: numpy.ndarray
-        Filtered image based on region of interest
-    """
-    # Grab the frame's height and width and center points
-    frameH, frameW = image.shape[:2]
-    centerX, centerY = frameW // 2, frameH // 2
-    # Define the circle equations
-    radius = int(centerY * coverage) if frameH < frameW else int(
-        centerX * coverage)
-    # Create a blank image of the same size as the original image
-    mask = np.zeros_like(image)
-    mask = cv.circle(mask, (centerX, centerY), radius, (255, 255, 255), -1)
-    # Subtraction
-    mask = cv.bitwise_and(mask, image)
-    # Return the result
-    return mask
+from filterROI import applyCircularMask
 
 
 def postProcessing(frame: np.ndarray, params: dict):
