056c1347d0fd26b92bec96e680a772847119c9fd
Ali Tourani <a.tourani1991@gmail.com>
2024-05-02 13:51:07 +0200

    [fix] - channel separator rgb

--- process.py
+++ process.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy as np
 from vision.postProcessing import postProcessing
-from vision.channelSeparatorRGB import channelSeparatorRGB
 from vision.concatImages import imageConcatHorizontal
+from vision.channelSeparatorRGB import channelSeparatorRGB
 from vision.alignImages import alignImages, alignImagesWithMatrix
 
 
