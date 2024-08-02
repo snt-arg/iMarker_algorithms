04e0db53cd6a6d3ec842edbcfe8fbcdb7d231341
Ali Tourani <a.tourani1991@gmail.com>
2024-08-02 17:59:47 +0200

    [fix] - resolve issue with setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import find_packages, setup
 
-setup(name='csr_detector',
-      version='1.0',
-      author='Ali Tourani',
-      description='CSR detector methodology using image registration',
-      url='https://github.com/snt-arg/csr_detector',
-      packages=find_packages(
-          include=['vision']),
-      install_requires=[
-          'numpy',
-          'opencv-python',
-      ],
-      )
+if __name__ == '__main__':
+    setup(name='csr_detector',
+          version='1.0',
+          author='Ali Tourani',
+          description='CSR detector methodology using image registration',
+          url='https://github.com/snt-arg/csr_detector',
+          packages=find_packages(
+              include=['vision']),
+          install_requires=[
+              'numpy',
+              'opencv-python',
+          ],
+          )
