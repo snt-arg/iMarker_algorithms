5c31133b2d0027dbbe12393fdb9bc8af006b0d64
Ali Tourani <a.tourani1991@gmail.com>
2024-09-11 18:37:49 +0200

    [fix] - resolve bug in checking isUsbCam

--- vision/postProcessing.py
+++ vision/postProcessing.py
@@ -21,7 +21,8 @@ def postProcessing(frame: np.ndarray, config: dict, isHSV: bool = False):
     """
     try:
         # Processing parameters
-        cfgUsbCam = config['sensor']['usbCam']
+        isUsbCam = config['mode']['runner'] == 'usb'
+        cfgUsbCam = config['sensor']['usbCam'] if isUsbCam else None
         cfgPostprocess = config['algorithm']['postprocess']
         cfgThreshold = cfgPostprocess['threshold']
         # Preparing the thresholding method
@@ -46,7 +47,6 @@ def postProcessing(frame: np.ndarray, config: dict, isHSV: bool = False):
             mask = cv.bitwise_not(mask)
 
         # Apply ROI
-        isUsbCam = config['mode']['runner'] == 'usb'
         if isUsbCam and cfgUsbCam['enableMask']:
             mask = applyCircularMask(
                 mask, cfgUsbCam['maskSize'], cfgPostprocess['invertBinary'])
